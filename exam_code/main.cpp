64339abc807389c3042fce70ab2fa480b11156d4
chunssam1999 <jslee6774@gmail.com>
2023-10-20 01:12:19 +0900

    Initial Commit

---
+++ exam_code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  exam_code
+//
+//  Created by Chun3 on 10/20/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
